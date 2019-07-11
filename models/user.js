beea07aef651ee58cbfbbb66162eb06514f6c03e
Amartya Mondal <hayyoulistentome@gmail.com>
2019-07-11 12:25:10 +0530

    feat: Add signup model

--- models/user.js
+++ models/user.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {type: String, required: true},
+  name: {type: String, required: false},
   email: {type: String, required: true},
+  password: {type: String, required: true},
   cart: {
     items: [{
       productId: {type: Schema.Types.ObjectId, ref: 'Product', required: true},
