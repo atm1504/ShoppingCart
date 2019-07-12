3b9ca945409e7af3c66f15ae319c1a7d61fd94d6
Travis CI <noreply+travis@fossasia.org>
2019-07-13 00:29:35 +0530

    feat: Add reset token

--- models/user.js
+++ models/user.js
@@ -6,6 +6,8 @@ const userSchema = new Schema({
   name: {type: String, required: false},
   email: {type: String, required: true},
   password: {type: String, required: true},
+  resetToken: String,
+  resetTokenExpiration: Date,
   cart: {
     items: [{
       productId: {type: Schema.Types.ObjectId, ref: 'Product', required: true},
