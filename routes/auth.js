f23863b5cc0052a3722119f9f4b32abfa75fb2c3
Amartya Mondal <hayyoulistentome@gmail.com>
2019-07-13 10:54:19 +0530

    feat: IMplement validation of forms

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const {check, body} = require('express-validator/check');
+const User = require('../models/user');
 
 const authController = require('../controllers/auth');
 
@@ -8,9 +10,44 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+    '/login',
+    [
+      body('email').isEmail().withMessage(
+          'Please enter a valid email address.'),
+      body('password', 'Password has to be valid.')
+          .isLength({min: 5})
+          .isAlphanumeric()
+    ],
+    authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+    '/signup',
+    [
+      check('email')
+          .isEmail()
+          .withMessage('Please Enter valid email.')
+          .custom(
+              (value,
+               {req}) => {return User.findOne({email: value}).then(userDoc => {
+                if (userDoc) {
+                  return Promise.reject(
+                      'Email Already Exists, please try a different one.');
+                }
+              })}),
+      body(
+          'password',
+          'Please enter valid password with atleast 5 characters in ALpha numeric form')
+          .isLength({min: 5})
+          .isAlphanumeric(),
+      body('confirmPassword').custom((value, {req}) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords field didn\'t match.');
+          }
+        return true;
+      })
+    ],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
