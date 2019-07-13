f8f213f2a8b9a7704cc55cd4a591ac84f1e5b892
Amartya Mondal <hayyoulistentome@gmail.com>
2019-07-14 02:31:52 +0530

    fix: All bugs have been fixed

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: {type: String, required: true},
   password: {type: String, required: true},
+  name: {type: String, required: false},
   resetToken: String,
   resetTokenExpiration: Date,
   cart: {
