650e0c1a0a1bb35d2da648876065e3d16afdc2d7
Amartya Mondal <hayyoulistentome@gmail.com>
2019-07-15 01:58:18 +0530

    feat: Run pdf file on the go

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const fileHelper = require('../util/file');
 
 const {validationResult} = require('express-validator/check');
 
@@ -135,6 +136,7 @@ exports.postEditProduct = (req, res, next) => {
         product.price = updatedPrice;
         product.description = updatedDesc;
         if (image) {
+          fileHelper.deleteFile(product.imageUrl);
           product.imageUrl = image.path;
           }
         return product.save().then(result => {
@@ -171,7 +173,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({_id: prodId, userId: req.user._id})
+  Product.findById(prodId)
+      .then(product => {
+        if (!product) {
+          return next(new Error('Product not found'));
+        }
+        fileHelper.deleteFile(product.imageUrl);
+        return Product.deleteOne({_id: prodId, userId: req.user._id})
+      })
       .then(() => {
         console.log('DESTROYED PRODUCT');
         res.redirect('/admin/products');
@@ -181,4 +190,6 @@ exports.postDeleteProduct = (req, res, next) => {
         error.httpStatusCode = 500;
         return next(error);
       });
+
+
 };
